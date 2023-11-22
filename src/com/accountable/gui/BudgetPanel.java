f3441a3e81283693f558207c42e64985dd5b9b02
AllAvocados <113918927+AllAvocados@users.noreply.github.com>
2023-11-21 19:34:40 -0600

    Banana

--- src/com/accountable/gui/BudgetPanel.java
+++ src/com/accountable/gui/BudgetPanel.java
@@ -1,55 +1,92 @@
 package com.accountable.gui;
 
+import com.accountable.CategoryUpdateListener;
+import com.accountable.core.Transaction;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class BudgetPanel extends JPanel {
 
-    private JTextField budgetNameField;
-    private JTextField budgetAmountField;
-    private JButton addBudgetButton;
-    private JList<String> budgetList; // Assuming the use of a String for demonstration
+    private JTextField categoryNameField;
+    private JTextField categoryAmountField;
+    private JButton addCategoryButton;
+    private JTable categoryTable;
+    private DefaultTableModel categoryModel;
+
+    private JComboBox<String> expenseCategoryComboBox; // ComboBox for spending categories
 
     public BudgetPanel() {
         setLayout(new BorderLayout());
 
-        JLabel headerLabel = new JLabel("Budget Management", JLabel.CENTER);
+        JLabel headerLabel = new JLabel("Budget", JLabel.CENTER);
         add(headerLabel, BorderLayout.NORTH);
 
+        // Panel to add new expenses
         JPanel budgetManagementPanel = new JPanel();
         budgetManagementPanel.setLayout(new GridLayout(0, 2)); // 0 rows, 2 columns
 
-        budgetManagementPanel.add(new JLabel("Budget Name:"));
-        budgetNameField = new JTextField(20);
-        budgetManagementPanel.add(budgetNameField);
+        budgetManagementPanel.add(new JLabel("Category Name:"));
+        categoryNameField = new JTextField(20);
+        budgetManagementPanel.add(categoryNameField);
 
-        budgetManagementPanel.add(new JLabel("Budget Amount:"));
-        budgetAmountField = new JTextField(20);
-        budgetManagementPanel.add(budgetAmountField);
+        budgetManagementPanel.add(new JLabel("Amount Allocated:"));
+        categoryAmountField = new JTextField(20);
+        budgetManagementPanel.add(categoryAmountField);
 
-        addBudgetButton = new JButton("Add Budget");
-        addBudgetButton.addActionListener(new AddBudgetActionListener());
-        budgetManagementPanel.add(addBudgetButton);
+        addCategoryButton = new JButton("Add Category");
+        addCategoryButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addBudget();
+            }
+        });
+        budgetManagementPanel.add(addCategoryButton);
 
-        budgetList = new JList<>(); // Placeholder for budget items
-        add(new JScrollPane(budgetList), BorderLayout.CENTER);
+        // Table to display expenses with the new "Spending Category" column
+        String[] columnNames = {"Category Name", "Amount Allocated"};
+        categoryModel = new DefaultTableModel(columnNames, 0);
+        categoryTable = new JTable(categoryModel);
+        add(new JScrollPane(categoryTable), BorderLayout.CENTER);
 
         add(budgetManagementPanel, BorderLayout.SOUTH);
     }
 
-    private class AddBudgetActionListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            // Placeholder for adding budget logic
-            String budgetName = budgetNameField.getText();
-            String budgetAmount = budgetAmountField.getText();
-            // TODO: Implement budget adding logic
-
-            JOptionPane.showMessageDialog(BudgetPanel.this,
-                    "Budget Added: " + budgetName + " - " + budgetAmount,
-                    "Budget Added", JOptionPane.INFORMATION_MESSAGE);
-        }
+    /*
+    // Call this method to update the categories from the Budget tab
+    public void updateCategoryComboBox(String[] categories) {
+        expenseCategoryComboBox.setModel(new DefaultComboBoxModel<>(categories));
     }
+    */
+
+    private void addBudget() {
+        String categoryName = categoryNameField.getText();
+        String categoryAmount = categoryAmountField.getText();
+
+        // Assuming you have validation in place for categoryAmount and spendingCategory
+        Transaction newExpense = new Transaction(categoryName, Double.parseDouble(categoryAmount), new Date(), true);
+
+        // Add new category to the table model including the spending category
+        categoryModel.addRow(new Object[]{
+                newExpense.getDescription(),
+                String.format("$%.2f", newExpense.getAmount()),
+        });
+
+        // Clear the input fields after adding the expense
+        categoryNameField.setText("");
+        categoryAmountField.setText("");
+
+        JOptionPane.showMessageDialog(BudgetPanel.this,
+                "Category Added: " + categoryName);
+                //"Expense Added", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    // Implementation of the CategoryUpdateListener interface
+    // Need to send the most updated Category list to the Expense Panel
+    //@Override
+
 }
--- src/com/accountable/gui/ExpensePanel.java
+++ src/com/accountable/gui/ExpensePanel.java
@@ -1,27 +1,32 @@
 package com.accountable.gui;
 
+import com.accountable.CategoryUpdateListener;
 import com.accountable.core.Transaction;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.DefaultListModel;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class ExpensePanel extends JPanel {
+public class ExpensePanel extends JPanel implements CategoryUpdateListener {
 
     private JTextField expenseNameField;
     private JTextField expenseAmountField;
     private JButton addExpenseButton;
-    private JList<Transaction> expenseList;
-    private DefaultListModel<Transaction> expenseModel;
+    private JTable expenseTable;
+    private DefaultTableModel expenseModel;
+
+    private JComboBox<String> expenseCategoryComboBox; // ComboBox for spending categories
 
     public ExpensePanel() {
         setLayout(new BorderLayout());
-        
+
         JLabel headerLabel = new JLabel("Expense Tracking", JLabel.CENTER);
         add(headerLabel, BorderLayout.NORTH);
 
+        // Panel to add new expenses
         JPanel expenseManagementPanel = new JPanel();
         expenseManagementPanel.setLayout(new GridLayout(0, 2)); // 0 rows, 2 columns
 
@@ -33,28 +38,68 @@ public class ExpensePanel extends JPanel {
         expenseAmountField = new JTextField(20);
         expenseManagementPanel.add(expenseAmountField);
 
+        // Label and ComboBox for the spending category
+        expenseManagementPanel.add(new JLabel("Spending Category:"));
+        expenseCategoryComboBox = new JComboBox<>(new DefaultComboBoxModel<>());
+        expenseManagementPanel.add(expenseCategoryComboBox);
+
         addExpenseButton = new JButton("Add Expense");
         addExpenseButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String expenseName = expenseNameField.getText();
-                String expenseAmount = expenseAmountField.getText();
-
-                // Create a new Transaction object with the current date and true for isExpense
-                Transaction newExpense = new Transaction(expenseName, Double.parseDouble(expenseAmount), new Date(), true);
-                expenseModel.addElement(newExpense); // Add new expense to the model
-
-                JOptionPane.showMessageDialog(ExpensePanel.this, 
-                        "Expense Added: " + newExpense, 
-                        "Expense Added", JOptionPane.INFORMATION_MESSAGE);
+                addExpense();
             }
         });
         expenseManagementPanel.add(addExpenseButton);
 
-        expenseModel = new DefaultListModel<>();
-        expenseList = new JList<>(expenseModel);
-        add(new JScrollPane(expenseList), BorderLayout.CENTER);
+        // Table to display expenses with the new "Spending Category" column
+        String[] columnNames = {"Expense Name", "Amount", "Spending Category", "Date"};
+        expenseModel = new DefaultTableModel(columnNames, 0);
+        expenseTable = new JTable(expenseModel);
+        add(new JScrollPane(expenseTable), BorderLayout.CENTER);
 
         add(expenseManagementPanel, BorderLayout.SOUTH);
+    }
+
+    // Call this method to update the categories from the Budget tab
+    public void updateCategoryComboBox(String[] categories) {
+        expenseCategoryComboBox.setModel(new DefaultComboBoxModel<>(categories));
+    }
+
+    private void addExpense() {
+        String expenseName = expenseNameField.getText();
+        String expenseAmount = expenseAmountField.getText();
+        String spendingCategory = (String) expenseCategoryComboBox.getSelectedItem(); // Get the selected spending category
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+        // Assuming you have validation in place for expenseAmount and spendingCategory
+        Transaction newExpense = new Transaction(expenseName, Double.parseDouble(expenseAmount), new Date(), true);
+
+        // Add new expense to the table model including the spending category
+        expenseModel.addRow(new Object[]{
+                newExpense.getDescription(),
+                String.format("$%.2f", newExpense.getAmount()),
+                spendingCategory, // Add the spending category here
+                dateFormat.format(newExpense.getDate())
+        });
+
+        // Clear the input fields after adding the expense
+        expenseNameField.setText("");
+        expenseAmountField.setText("");
+
+        JOptionPane.showMessageDialog(ExpensePanel.this,
+                "Expense Added: " + expenseName,
+                "Expense Added", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    // Implementation of the CategoryUpdateListener interface
+    //@Override
+    public void updateCategories(String[] categories) {
+        // Update the combo box with the new categories
+        expenseCategoryComboBox.setModel(new DefaultComboBoxModel<>(categories));
+        // Optionally select the first item by default
+        if (categories.length > 0) {
+            expenseCategoryComboBox.setSelectedIndex(0);
+        }
     }
 }
