effe6172537388cb4c45745551cb5db042816cf6
Joy <anointiyaebeasley1722@gmail.com>
2023-11-25 22:14:17 -0600

    Registration update

--- src/com/accountable/gui/Dialogs/RegistrationDialog.java
+++ src/com/accountable/gui/Dialogs/RegistrationDialog.java
@@ -2,17 +2,20 @@
 package com.accountable.gui.Dialogs;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import com.accountable.core.User; // Assuming User class exists in the core package for user creation
+import com.accountable.core.User;
 
 public class RegistrationDialog extends JDialog {
 
-    private JTextField usernameField;
-    private JPasswordField passwordField;
-    private JButton registerButton;
-    private JButton cancelButton;
+    private final JTextField usernameField;
+    private final JPasswordField passwordField;
+    private final JButton registerButton;
+
+    private boolean registrationSuccessful = false;
 
     public RegistrationDialog(JFrame parent) {
         super(parent, "Register", true);
@@ -36,36 +39,99 @@ public class RegistrationDialog extends JDialog {
         });
         add(registerButton);
 
-        cancelButton = new JButton("Cancel");
-        cancelButton.addActionListener(e -> dispose());
+        JButton cancelButton = new JButton("Cancel");
+        cancelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                onCancelClicked();
+            }
+        });
         add(cancelButton);
+
+        registerButton.setEnabled(false);
+
+        DocumentListener documentListener = new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                checkInputValidity();
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                checkInputValidity();
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                checkInputValidity();
+            }
+        };
+
+        usernameField.getDocument().addDocumentListener(documentListener);
+        passwordField.getDocument().addDocumentListener(documentListener);
     }
 
-    boolean registrationSuccessful = false;
+    private void checkInputValidity() {
+        String username = usernameField.getText();
+        char[] password = passwordField.getPassword();
+        boolean isValid = !username.isEmpty() && password.length >= 8 && containsNumber(password) && containsSpecialChar(password) && containsUpperCase(password);
+
+        registerButton.setEnabled(isValid);
+    }
+
+    private boolean containsNumber(char[] password) {
+        for (char c : password) {
+            if (Character.isDigit(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean containsSpecialChar(char[] password) {
+        String specialChars = "!@#$%^&*()-_=+[]{}|;:'\",.<>/?";
+        for (char c : password) {
+            if (specialChars.indexOf(c) != -1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean containsUpperCase(char[] password) {
+        for (char c : password) {
+            if (Character.isUpperCase(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     private void registerUser() {
         String username = usernameField.getText();
         char[] password = passwordField.getPassword();
 
-        // Basic validation (more comprehensive validation can be implemented)
-        if (username.isEmpty() || password.length == 0) {
-            JOptionPane.showMessageDialog(this, "Username and password cannot be empty", "Error", JOptionPane.ERROR_MESSAGE);
-            return;
-        }
-
-        // Assuming User class has a method to create a new user
         boolean userCreated = User.createUser(username, new String(password));
 
         if (userCreated) {
             registrationSuccessful = true;
             JOptionPane.showMessageDialog(this, "Registration successful", "Success", JOptionPane.INFORMATION_MESSAGE);
-            User.createUser(username, new String(password));
             dispose();
         } else {
             registrationSuccessful = false;
-            JOptionPane.showMessageDialog(this, "Registration failed", "Error", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(this, "Registration failed. Please choose a different username.", "Error", JOptionPane.ERROR_MESSAGE);
         }
+    }
 
+    private void onCancelClicked() {
+        int confirmResult = JOptionPane.showConfirmDialog(this,
+                "Are you sure you want to cancel registration?",
+                "Confirm Cancel",
+                JOptionPane.YES_NO_OPTION);
+
+        if (confirmResult == JOptionPane.YES_OPTION) {
+            dispose();
+        }
     }
 
     public boolean isRegistrationSuccessful() {
--- src/com/accountable/gui/LoginWindow.java
+++ src/com/accountable/gui/LoginWindow.java
@@ -16,86 +16,96 @@ import java.util.ArrayList;
 
 public class LoginWindow extends JFrame {
 
-    private JTextField usernameField;
-    private JPasswordField passwordField;
-    private JButton loginButton;
-    private JButton registerButton;
+    private final JTextField usernameField;
+    private final JPasswordField passwordField;
+    private final JButton loginButton;
+    private final JButton registerButton;
 
-    public LoginWindow() {
-        setTitle("Login");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new BorderLayout(5, 5));
-        setSize(400, 200);
-        setLocationRelativeTo(null); // Center the window
 
-        JPanel inputPanel = new JPanel(new GridLayout(2, 2, 5, 5));
-        usernameField = new JTextField();
-        passwordField = new JPasswordField();
-        inputPanel.add(new JLabel("   Username:"));
-        inputPanel.add(usernameField);
-        inputPanel.add(new JLabel("   Password:"));
-        inputPanel.add(passwordField);
+// Import statements remain unchanged
 
-        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 5, 5));
-        loginButton = new JButton("Login");
-        loginButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                login();
-            }
-        });
 
-        registerButton = new JButton("Register");
-        registerButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                onRegisterClicked();
-            }
-        });
+        public LoginWindow() {
+            setTitle("Login");
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            setLayout(new BorderLayout(5, 5));
+            setSize(400, 200);
+            setLocationRelativeTo(null); // Center the window
 
-        buttonPanel.add(loginButton);
-        buttonPanel.add(registerButton);
+            JPanel inputPanel = new JPanel(new GridLayout(2, 2, 5, 5));
+            usernameField = new JTextField();
+            passwordField = new JPasswordField();
+            inputPanel.add(new JLabel("   Username:"));
+            inputPanel.add(usernameField);
+            inputPanel.add(new JLabel("   Password:"));
+            inputPanel.add(passwordField);
 
-        add(inputPanel, BorderLayout.CENTER);
-        add(buttonPanel, BorderLayout.SOUTH);
-    }
+            JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 5, 5));
+            loginButton = new JButton("Login");
+            loginButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    login();
+                }
+            });
 
-    // Method to handle login logic
-    private void login() {
-        String username = usernameField.getText();
-        String password = new String(passwordField.getPassword());
+            registerButton = new JButton("Register");
+            registerButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    onRegisterClicked();
+                }
+            });
 
-        if (Authentication.authenticate(username, password)) {
-            dispose();
-            MainWindow mainWindow = new MainWindow(); // Replace with your main window class
-            mainWindow.setVisible(true);
-        } else {
-            JOptionPane.showMessageDialog(this, "Invalid username or password.", "Login Failed", JOptionPane.ERROR_MESSAGE);
+            buttonPanel.add(loginButton);
+            buttonPanel.add(registerButton);
+
+            add(inputPanel, BorderLayout.CENTER);
+            add(buttonPanel, BorderLayout.SOUTH);
         }
-    }
 
-    // Method to handle registration logic
-    private void onRegisterClicked() {
-        RegistrationDialog registrationDialog = new RegistrationDialog(this);
-        registrationDialog.setVisible(true);
+        private void login() {
+            String username = usernameField.getText();
+            String password = new String(passwordField.getPassword());
 
-        if (registrationDialog.isRegistrationSuccessful()) {
-            // After successful login or registration
-            MainWindow mainWindow = new MainWindow();
-            mainWindow.setVisible(true); // This will show the main application window
-            startNewUserWorkflow(mainWindow);
+            try {
+                if (Authentication.authenticate(username, password)) {
+                    dispose();
+                    MainWindow mainWindow = new MainWindow();
+                    mainWindow.setVisible(true);
+                } else {
+                    JOptionPane.showMessageDialog(this, "Invalid username or password.", "Login Failed", JOptionPane.ERROR_MESSAGE);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-    }
 
-    // Method gets called after a new user has successfully registered.
-    private void startNewUserWorkflow(MainWindow mainWindow) {
-        ProjectedIncomeDialog projectedIncomeDialog = new ProjectedIncomeDialog(mainWindow, true);
-        projectedIncomeDialog.setVisible(true);
+        private void onRegisterClicked() {
+            RegistrationDialog registrationDialog = new RegistrationDialog(this);
 
-        // Continue with category setup only if the income was entered
-        if (projectedIncomeDialog.getProjectedIncome() != null) {
-            SpendingCategoriesSetupDialog categoriesDialog = new SpendingCategoriesSetupDialog(this, true, projectedIncomeDialog.getProjectedIncome());
-            categoriesDialog.setVisible(true);
+            try {
+                registrationDialog.setLocationRelativeTo(this);
+                registrationDialog.setVisible(true);
+
+                if (registrationDialog.isRegistrationSuccessful()) {
+                    MainWindow mainWindow = new MainWindow();
+                    mainWindow.setVisible(true);
+                    startNewUserWorkflow(mainWindow);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void startNewUserWorkflow(MainWindow mainWindow) {
+            ProjectedIncomeDialog projectedIncomeDialog = new ProjectedIncomeDialog(mainWindow, true);
+            projectedIncomeDialog.setVisible(true);
+
+            // Continue with category setup only if the income was entered
+            if (projectedIncomeDialog.getProjectedIncome() != null) {
+                SpendingCategoriesSetupDialog categoriesDialog = new SpendingCategoriesSetupDialog(this, true, projectedIncomeDialog.getProjectedIncome());
+                categoriesDialog.setVisible(true);
 
             /*
             // Continue with expense entry only if the categories were set up
@@ -103,18 +113,18 @@ public class LoginWindow extends JFrame {
                 ExpenseEntryDialog expenseDialog = new ExpenseEntryDialog(this, true, new ArrayList<>(categoriesDialog.getCategoryAllocations().keySet()));
                 expenseDialog.setVisible(true);
             */
-            // Handle the expense entry and update the budget as needed
-        }
+                // Handle the expense entry and update the budget as needed
+            }
 
 //setupDialog.isCategoriesSet(){
 
-        //CHECK
+            //CHECK
 
 
-    //}
-        //ADD THE BELOW TO THE .isCategoriesSet() method
-        JOptionPane.showMessageDialog(this, "Spending Categories configured.", "Configuration Complete", JOptionPane.INFORMATION_MESSAGE);
+            //}
+            //ADD THE BELOW TO THE .isCategoriesSet() method
+            JOptionPane.showMessageDialog(this, "Spending Categories configured.", "Configuration Complete", JOptionPane.INFORMATION_MESSAGE);
 
+        }
     }
-}
 
