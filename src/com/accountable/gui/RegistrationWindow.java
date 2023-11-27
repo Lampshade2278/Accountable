cad190eb77cd4261850b2eb8193ce2b6ae68c22c
Dark1820 <zac.castaneda@gmail.com>
2023-11-26 22:47:20 -0600

    Password Criteria

--- src/com/accountable/gui/RegistrationWindow.java
+++ src/com/accountable/gui/RegistrationWindow.java
@@ -17,11 +17,13 @@ public class RegistrationWindow extends JFrame {
         setLayout(new BorderLayout(10, 10));
 
         // Adjust the grid and padding to match the LoginWindow
-        JPanel inputPanel = new JPanel(new GridLayout(2, 2, 20, 20));
+        JPanel inputPanel = new JPanel(new GridLayout(3, 2, 20, 20));
         inputPanel.setBorder(BorderFactory.createEmptyBorder(100, 50, 100, 50)); // Padding adjusted for symmetry
 
         // Use a consistent font for labels and text fields
         Font labelFont = new Font("Arial", Font.BOLD, 16);
+
+        Font critFont = new Font("Arial", Font.BOLD, 12);
 
         JLabel usernameLabel = new JLabel("Username:");
         usernameLabel.setFont(labelFont);
@@ -31,6 +33,14 @@ public class RegistrationWindow extends JFrame {
         usernameField.setFont(labelFont); // Set the text field font to be consistent
         usernameField.setPreferredSize(new Dimension(200, 30)); // Set the preferred size to make it longer
         inputPanel.add(usernameField);
+
+        JLabel passwordBuffer = new JLabel("        ");
+        passwordBuffer.setFont(labelFont);
+        inputPanel.add(passwordBuffer);
+
+        JLabel passwordCriteria = new JLabel("Password must be at least 8 character long.");
+        passwordCriteria.setFont(critFont);
+        inputPanel.add(passwordCriteria);
 
         JLabel passwordLabel = new JLabel("Password:");
         passwordLabel.setFont(labelFont);
@@ -43,6 +53,7 @@ public class RegistrationWindow extends JFrame {
 
         registerButton = new JButton("Register");
         registerButton.addActionListener(e -> registerUser(usernameField.getText(), new String(passwordField.getPassword())));
+
         registerButton.setFont(labelFont); // Use the same font as the labels for consistency
 
         JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
@@ -55,11 +66,17 @@ public class RegistrationWindow extends JFrame {
         setLocationRelativeTo(null);
     }
 
-
+    boolean isSignUpSuccessful;
     private void registerUser(String username, String password) {
         // Registration logic remains the same
-        boolean isSignUpSuccessful = Authentication.signUp(username, password);
-
+        if(password.length()>=8){
+        isSignUpSuccessful = Authentication.signUp(username, password);}
+        else {
+            JOptionPane.showMessageDialog(this,
+                    "Registration failed. Password did not fit the criteria, or User may already exist.",
+                    "Registration Error",
+                    JOptionPane.ERROR_MESSAGE);
+        }
         // Custom font to match the other windows
         UIManager.put("OptionPane.messageFont", new Font("Arial", Font.BOLD, 16));
         UIManager.put("OptionPane.buttonFont", new Font("Arial", Font.BOLD, 12));
@@ -75,12 +92,6 @@ public class RegistrationWindow extends JFrame {
                 MainWindow mainWindow = new MainWindow(username);
                 mainWindow.setVisible(true);
             });
-        } else {
-            // Customized JOptionPane to match the RegistrationWindow style
-            JOptionPane.showMessageDialog(this,
-                    "Registration failed. User may already exist.",
-                    "Registration Error",
-                    JOptionPane.ERROR_MESSAGE);
         }
     }
 
